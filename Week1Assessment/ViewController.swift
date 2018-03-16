5ba24a3a1fa24bba0933d6051178ed1f7b18d343
Noah Smith <noahsmith@Interapts-MacBook-Pro-34.local>
2018-03-16 14:48:39 -0400

    finished project

---
+++ Week1Assessment/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Week1Assessment
+//
+//  Created by Noah Smith on 3/16/18.
+//  Copyright © 2018 Noah Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.purple
+}
+
+    @IBAction func buttonPressed() {
+        let textInput: String
+        textInput = 
+    }
+}
